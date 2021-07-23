b1449473c7eb5f6c2f789ee7770a8b3a1128a3d1
CarlMariaMikage <dassayan0013@gmail.com>
2021-07-23 09:11:30 +0530

    0

---
+++ rmrc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rmrc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
